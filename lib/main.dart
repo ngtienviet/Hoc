fe833906590b9655fe107f3be48b39d740b44b11
VIT <dtc2154801030140@ictu.edu.vn>
2024-11-26 11:05:23 +0700

    sửa 1 không thay đổi gi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MaterialApp(home: app(),));
+}
+class app extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
 }
